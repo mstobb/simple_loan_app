44d108e39ab6e6e1fa722af2a964d37b4fd0d99a
Michael Stobb <mstobb@gmail.com>
2021-07-15 12:00:15 -0500

    Added basic file contents

---
+++ script.js
@@ -0,0 +1,25 @@
+function showResult(elem) {
+  document.getElementById("showresulthere").innerHTML = Number(elem.value) + 2;
+}
+
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
+
+// Start file download.
+document.getElementById("dwn-btn").addEventListener("click", function(){
+    // Generate download of hello.txt file with some content
+    var text = document.getElementById("text-val").value;
+    var filename = "hello.txt";
+
+    download(filename, text);
+}, false);
